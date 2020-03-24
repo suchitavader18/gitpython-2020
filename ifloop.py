d221d6c842367137058485b80db9c2989cea9ef1
SuchitaVader <suchitavader18@gmail.com>
2020-03-24 14:02:05 +0530

    First Commit

---
+++ ifloop.py
@@ -0,0 +1,11 @@
+# If-else statement:
+
+user_info=input("Enter the age: ")
+user_info=int(user_info)
+
+if user_info<=14:
+    print("You can watch movie")
+else:
+    print("You can not watch movie")
+
+
